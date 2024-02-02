b99f08314502e3b3cbf0480045d01e361004c5b0
DomLaw82 <domlaw82@users.noreply.github.com>
2024-02-02 16:56:27 +0000

    testing run_data_prep

--- app/predict/win_prediction/win_prediction.py
+++ app/predict/win_prediction/win_prediction.py
@@ -1,12 +1,12 @@
-from win_prediction.data_preparation import run_data_prep
-from win_prediction.data_modelling_one import run_data_modelling_part_one
-from win_prediction.data_modelling_two import run_data_modelling_part_two
+from data_preparation import run_data_prep
+from data_modelling_one import run_data_modelling_part_one
+from data_modelling_two import run_data_modelling_part_two
 import pandas as pd
 
 if __name__ == '__main__':
 	run_data_prep()
 
 
-	data = pd.read_csv('../match_and_form_data.csv')
-	results = run_data_modelling_part_one(data)
-	all_results = run_data_modelling_part_two(data)
+	# data = pd.read_csv('../match_and_form_data.csv')
+	# results = run_data_modelling_part_one(data)
+	# all_results = run_data_modelling_part_two(data)
