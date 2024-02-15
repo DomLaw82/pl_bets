fac962babc42186ac2ac36273fc8f8ebd3fd14f8
DomLaw82 <domlaw82@users.noreply.github.com>
2024-02-15 14:56:20 +0000

    editing readme and removing redundant files

--- app/backend/database/main.py
+++
@@ -1,8 +0,0 @@
-# read schedule at 00:01 every day
-# game_today = if current day matches date of the match on the schedule
-# if not game_today, sleep until 00:00
-# if game_today:
-	# generate match ids
-	# collect:
-		# historical data for all players on team (average per 90 stats for every player on all the teams)
-		# form data for all players on team (last 5 games for each player on all the teams)
