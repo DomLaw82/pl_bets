658864663ab816e217213ba69b50d3555bf77893
DomLaw82 <domlaw82@users.noreply.github.com>
2024-04-25 00:11:52 +0100

    adding origin

--- app/backend/api/api.py
+++ app/backend/api/api.py
@@ -262,7 +262,7 @@ def update_historic_per_ninety(content:str):
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://frontend:3000", "http://localhost:3000"],  supports_credentials=True)
+CORS(app, origins=["http://frontend:3000", "http://10.127.67.163:3001", "http://localhost:3000"],  supports_credentials=True)
 rebar.init_app(app)
 
 if __name__ == '__main__':
