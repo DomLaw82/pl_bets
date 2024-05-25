c06e8787815a33b4bf978e2e817d66a9551e86de
DomLaw82 <domlaw82@users.noreply.github.com>
2024-05-25 04:50:35 +0100

    conditional run of weekly fixture outcome prediction

--- app/predict/win_prediction.py
+++ app/predict/win_prediction.py
@@ -7,11 +7,15 @@ import pandas as pd
 def run_win_prediction(home_team_id: str = None, away_team_id: str = None):
 	run_data_prep()
 	data = pd.read_csv('../files/match_and_form_data.csv')
+	
 	results = run_data_modelling_part_one_logistic_regression(data)
 	print(results)
+
 	all_results = run_data_modelling_part_two(data)
-	fixture_odds = predict_fixture_outcome_odds(data, home_team_id=home_team_id, away_team_id=away_team_id)
-	return fixture_odds
+
+	if home_team_id and away_team_id:
+		fixture_odds = predict_fixture_outcome_odds(data, home_team_id=home_team_id, away_team_id=away_team_id)
+		return fixture_odds
 
 if __name__ == '__main__':
 	run_win_prediction()
