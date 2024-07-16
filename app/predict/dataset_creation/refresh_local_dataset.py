19c9273d009642f6961737af2f9b42aa46907044
DomLaw82 <domlaw82@users.noreply.github.com>
2024-07-16 15:36:35 +0100

    update refresh script

--- app/predict/dataset_creation/refresh_local_dataset.py
+++ app/predict/dataset_creation/refresh_local_dataset.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from db_connection import SQLConnection
-from create_dataset import create_dataset
+from create_dataset import create_training_dataset
 import os
+from dotenv import load_dotenv
 from app_logger import FluentLogger
 
-db = SQLConnection(os.environ.get("POSTGRES_USER"), os.environ.get("POSTGRES_PASSWORD"), os.environ.get("POSTGRES_CONTAINER"), os.environ.get("POSTGRES_PORT"), os.environ.get("POSTGRES_DB"))
+load_dotenv()
+
+db = SQLConnection(os.environ.get("POSTGRES_USER"), os.environ.get("POSTGRES_PASSWORD"), "localhost" or os.environ.get("POSTGRES_CONTAINER"), os.environ.get("POSTGRES_PORT"), os.environ.get("POSTGRES_DB"))
 logger = FluentLogger("predict-refresh_local_dataset").get_logger()
 
 def recreate_local_dataset():
 	try:
-		df = create_dataset()
+		df = create_training_dataset(db)
 		df.to_csv("../files/final_combined_dataframe.csv", index=False)
 		logger.info("Local dataset recreated successfully")
 	except Exception as e:
