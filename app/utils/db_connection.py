70a83a333fa7a707c6a92e8e42b861f1c20ef9dc
DomLaw82 <domlaw82@users.noreply.github.com>
2024-08-10 11:12:28 +0100

    adding linting

--- app/utils/db_connection.py
+++ app/utils/db_connection.py
@@ -20,7 +20,7 @@ class SQLConnection:
             self.engine = sqlalchemy.create_engine(self.connection_string, isolation_level='AUTOCOMMIT')
         return self.engine.connect()
 
-    def get_df(self, query: str):
+    def get_df(self, query: str) -> pd.DataFrame:
         """Query the database and return a DataFrame, managing context internally."""
         with self.connect() as conn:
             try:
@@ -29,7 +29,7 @@ class SQLConnection:
             except Exception as e:
                 raise ValueError(e) # Optionally re-raise the exception after logging
 
-    def get_list(self, query: str):
+    def get_list(self, query: str) -> list:
         """Execute a query and return a list, managing context internally."""
         try:
             with self.connect() as conn:
@@ -37,21 +37,21 @@ class SQLConnection:
         except Exception as e:
             raise ValueError(e)
 
-    def get_dict(self, query: str):
+    def get_dict(self, query: str) -> list[dict]:
         """Execute a query and return a list of dictionaries, managing context internally."""
         with self.connect() as conn:
             return self._execute_query(conn, query, fetch='dict')
 
-    def execute(self, query):
+    def execute(self, query) -> None:
         """Execute a non-return query, managing context internally."""
         with self.connect() as conn:
             try:
                 conn.execute(sqlalchemy.text(query))
             except SQLAlchemyError as e:
                 print('Database error:', e)
-                raise
+                raise e
 
-    def _execute_query(self, conn, query: str, fetch='all'):
+    def _execute_query(self, conn, query: str, fetch='all') -> list:
         """Helper method to execute queries and manage fetch types."""
         try:
             result = conn.execute(sqlalchemy.text(query))
