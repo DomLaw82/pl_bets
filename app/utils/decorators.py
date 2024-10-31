f70ebd681bb6ed81f87300a37a661cc439880bf5
DomLaw82 <domlaw82@users.noreply.github.com>
2024-10-31 12:19:31 +0000

    Refactor decorators.py to add exception handling

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from app_logger import FluentLogger
 
 def time_this_function(func):
 
@@ -8,5 +9,16 @@ def time_this_function(func):
 		end = datetime.now()
 		print(f"Function {func.__name__} took {end - start} to run.")
 		return result
+	
+	return wrapper
+
+def handle_exceptions(func):
+
+	def wrapper(*args, **kwargs):
+		try:
+			result = func(*args, **kwargs)
+			return result
+		except Exception as e:
+			print(f"An error occurred: {e}")
 	
 	return wrapper
