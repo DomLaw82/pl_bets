1489dbc0f2cff7b4fc5c11972079cfdcb9d85b02
DomLaw82 <domlaw82@users.noreply.github.com>
2024-10-31 12:41:58 +0000

    Refactor FluentLogger class to improve initialization and logging configuration

--- app/utils/app_logger.py
+++ app/utils/app_logger.py
@@ -8,7 +8,7 @@ from define_environment import load_correct_environment_variables
 load_correct_environment_variables()
 
 class FluentLogger:
-    def __init__(self, tag, host=os.getenv("FLUENT_APP_HOST_NAME"), port=24224, level=logging.DEBUG):
+    def __init__(self, tag: str, host: str = os.getenv("FLUENT_APP_HOST_NAME"), port: int = 24224, level=logging.DEBUG):
         """
         Initialize the Fluent Logger.
 
@@ -18,13 +18,18 @@ class FluentLogger:
             port (int): Port number on which Fluentd server is listening.
             level (logging.level): Minimum logging level for the logger.
         """
-        self.logger = logging.getLogger(tag)
-        self.logger.setLevel(level)
+        self.tag = tag
+        self.host = host
+        self.port = port
+        self.level = level
+        
+        self.logger = logging.getLogger(self.tag)
+        self.logger.setLevel(self.level)
         self.logger.propagate = False
 
         # Fluentd handler
-        fluent = fluent_handler.FluentHandler(tag, host=host, port=port)
-        fluent.setLevel(level)
+        fluent = fluent_handler.FluentHandler(self.tag, host=self.host, port=self.port)
+        fluent.setLevel(self.level)
         fluent.setFormatter(
             fluent_handler.FluentRecordFormatter({
 				'host': '%(hostname)s',
@@ -43,6 +48,12 @@ class FluentLogger:
         # # Add handlers to the logger
         # self.logger.addHandler(console)
         self.logger.addHandler(fluent)
+
+    def __repr__(self):
+        return f"FluentLogger(tag={self.tag}, host={self.host}, port={self.port}, level={self.level})"
+
+    def __str__(self):
+        return f"FluentLogger for module {self.logger.name}"
 
     def get_logger(self):
         """
